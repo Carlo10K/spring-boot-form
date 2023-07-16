67670b27e3f70dcc12965c62aed9c00a9af256d5
Carlo10K <carlosortegadiaz50@gmail.com>
2023-07-16 11:54:47 -0600

    manejo de errores con alias

--- src/main/java/com/example/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,15 +21,16 @@ public class FormController {
     public String form(Model model){
         Usuario usuario = new Usuario();            //para enviar un usuario nulo por default a la vista
         model.addAttribute("titulo", "formulario usuario");
-        model.addAttribute("usuario", usuario);         //enviando un usuario nulo para que cuando se
+        model.addAttribute("user", usuario);         //enviando un usuario nulo para que cuando se
                                                                     //acceda a el desde la vista por primera vez no de error
         return "form";
     }
 
     @PostMapping("/form")  /*@Valid para usar la libreria de validacion jakarta, El objeto binding result de spring obtiene el resultado
                             de la validacion y siempre debe ir despues del objeto a validar, el objeto Usuario se envia a la vista
-                            y para acceder a el se usa el mismo nombre de la clase pero con inicial mayuscula en este caso usuario*/
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model /*@RequestParam String username, @RequestParam String password, @RequestParam String email*/){
+                            y para acceder a el se usa el mismo nombre de la clase pero con inicial mayuscula en este caso usuario
+                            @ModelAttribute para cambiar el nombre con el que queremos pasar a la vista*/
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model /*@RequestParam String username, @RequestParam String password, @RequestParam String email*/){
 
         /* si la clase tiene los mismos nombres no es necesario inicializarla y se puede
         poblar la clase
